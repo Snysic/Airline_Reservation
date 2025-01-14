33b365be520d51327b9d80b986651f6c1bbc5fff
Maria Vasilenko <snysic1987@gmail.com>
2025-01-14 18:05:12 +0100

    first commit

---
+++ src/main/java/airlinereservation/project/Airlinereservation/AirlineReservationApplication.java
@@ -0,0 +1,13 @@
+package airlinereservation.project.Airlinereservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlineReservationApplication.class, args);
+	}
+
+}
